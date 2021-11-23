49f368bf823c96a32d3093dda3eb8c8d4954169a
Alysson-Silva <alyssonlogar123@gmail.com>
2021-11-23 20:43:26 -0300

    base de dados completado

--- cadeiaAlimentar.pl
+++ cadeiaAlimentar.pl
@@ -1,38 +1,73 @@
-%Fatos
-
-% Produtores: aqueles que produzem o proprio alimento, seja através da
-% fotossíntese ou quimiossíntese.
-%
 % Consumidores primarios: organismos que se alimentam diretamente dos
 % Produtores.
-%
+
 % Consumidores secundarios: são os seres vivos que se alimentam dos
 % consumidores primários.
 
+% Produtores: aqueles que produzem o proprio alimento, seja através da
+% fotossíntese ou quimiossíntese.
 
-vegetacao(arbusto).
-vegetacao(arvore).
-vegetacao(capim).
+%Produtores
+produtor(arvore).
+produtor(vegetacao).
+
+% Consumidores
+animal(girafa).
+animal(elefante).
+animal(cupim).
+animal(gafanhoto).
+animal(javali).
+animal(antilope).
+animal(zebra).
+animal(formiga).
+animal(gazela).
+animal(gnu).
+animal(rato).
+animal(guepardo).
+animal(serval).
+animal(suricato).
+animal(pangolim).
+animal(porcoformigueiro).
+animal(abutre).
+animal(hiena).
+animal(pantera).
+animal(lince).
+animal(leao).
+
+
+%Fatos
+come(girafa, arvore).
+come(elefante, arvore).
+come(cupim, vegetacao).
+come(gafanhoto, vegetacao).
+come(javali, vegetacao).
+come(antilope, vegetacao).
+come(zebra, vegetacao).
+come(formiga, vegetacao).
+come(gazela, vegetacao).
+come(gnu, vegetacao).
+come(rato, vegetacao).
+come(guepardo, girafa), come(guepardo, elefante);
+come(serval, cupim), come(serval, gafanhoto), come(serval, rato), come(serval, gnu).
+come(suricato, gafanhoto).
+come(pangolim, formiga).
+come(porcoformigueiro, cupim), come(porcoformigueiro, formiga).
+come(abutre, guepardo), come(abutre, girafa), come(abutre, elefante), come(abutre, zebra), come(abutre, gazela).
+come(hiena, suricato), come(hiena, elefante), come(hiena, zebra), come(hiena, porcoformigueiro), come(hiena, gnu), come(hiena, pantera).
+come(pantera, javali), come(pantera, zebra), come(pantera, gnu).
+come(lince, rato), come(lince, gazela).
+come(leao, guepardo), come(leao, girafa), come(leao, javali), come(leao, antilope), come(leao, zebra), come(leao, hiena), come(leao, gnu), come(leao, pantera), come(leao, gazela), come(leao, lince).
+
 
 % Aqueles que se alimentam de fontes vegetais:
-herbivoro(gazela).
-herbivoro(gafanhoto).
-herbivoro(cupim).
 
 % Seres vivos que consomem alimentos de fonte vegetal e animal:
-onivoro(rato).
-onivoro(javali).
-onivoro(suricato).
+% Onivoros:
+onivoro(X):- come(X,Y),come(X,Z),animal(Y),vegetacao(Z).
 
 % Animais que se alimenam da carne de outros animais.
-carnivoro(abutre).
-carnivoro(hiena).
-carnivoro(guepardo).
 
 
-%Regras
-%come(X,Y) :-
-    come(Y, Z).
 
 
 
